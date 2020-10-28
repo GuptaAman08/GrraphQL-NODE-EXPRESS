f83d97b09be4dd95d8ab00014bb1184d4e2e446a
Aman Gupta <2015aman.a@gmail.com>
2020-10-28 11:28:33 +0530

    Removing old Auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,10 +1,10 @@
 const jwt = require('jsonwebtoken')
-const { customError } = require('../utils/generate-custom-erros')
 
 module.exports = (req, res, next) => {
     const authHeader = req.get("Authorization")
     if (!authHeader){
-        throw customError("Authentication Failed", 401)
+        req.isAuth = false
+        return next()
     }
     const token = authHeader.split(" ")[1]
     let decodeToken;
@@ -12,18 +12,18 @@ module.exports = (req, res, next) => {
     try {
         decodeToken = jwt.verify(token, process.env.JSON_WEB_TOKEN_SECRET_KEY)
     } catch (err) {
-        // When any technical error occured
-        if (!err.statusCode){
-            err.statusCode = 500
-        }
-        throw err
+        req.isAuth = false
+        return next()
     }
 
     // This if block gets execute when jwt cannot decode the token
     if (!decodeToken){
-        throw customError("Authentication Failed", 401)
+        req.isAuth = false
+        return next()
     }
 
     req.userId = decodeToken.userId
+    req.isAuth = true
+    // console.log(req.isAuth)
     next()
 }
