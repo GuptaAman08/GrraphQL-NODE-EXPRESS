c9ef418693d9d058b146713ba8738a3459b07043
Aman Gupta <2015aman.a@ves.ac.in>
2020-10-28 11:35:55 +0530

    CHanging app js file for using  graphql

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const multer = require('multer');
 const mongoose = require('mongoose')
+const { graphqlHTTP } = require('express-graphql')
 
-const feedRoutes = require('./routes/feed');
-const authRoutes = require('./routes/auth');
+const graphqlSchema = require("./graphql/schema")
+const graphqlResolver = require("./graphql/resolvers")
+const authMiddleware = require("./middleware/auth")
+const deleteImage = require('./utils/delete-image')
 
 require('dotenv').config()
 
@@ -38,14 +41,55 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    if (req.method === "OPTIONS"){
+        return res.sendStatus(200)
+    }
+    
     next();
 });
 
-app.use('/feed', feedRoutes);
-app.use('/auth', authRoutes);
+app.use(authMiddleware)
+
+app.put("/post-image", (req, res, next) => {
+    if (!req.isAuth){
+        throw new Error("Not Authenticated")
+    }
+
+    if (!req.file){
+        return res.status(200).json({message: "File not provided"})
+    }
+
+    if (req.body.oldPath){
+        deleteImage(req.body.oldPath)
+    }
+
+    return res.status(201).json({message: "File Stored", filePath: "/" + req.file.path.replace(/\\/g, "/")})
+})
+
+app.use("/graphql", graphqlHTTP({
+    schema: graphqlSchema,
+    rootValue: graphqlResolver,
+    graphiql: true,
+    customFormatErrorFn(err) {
+        if (!err.originalError){
+            return err
+        }
+        const data = err.originalError.data
+        const message = err.originalError.message || "An Error Occured"
+        const statusCode = err.originalError.statusCode || 500
+
+        return {
+            message: message,
+            status: statusCode,
+            data: data
+        }
+    }
+}))
 
 // Express buitl-in special erro handling middleware
 app.use((error, req, res, next) => {
+    console.log('Inside error handling functon')
     const status = error.statusCode || 500
     const message = error.message
     const data = error.data
@@ -57,15 +101,8 @@ app.use((error, req, res, next) => {
 
 mongoose.connect('mongodb+srv://aman:snZ5L0a4JMbsXqWG@primary.u62r1.mongodb.net/feeds?retryWrites=true&w=majority', {useUnifiedTopology: true, useNewUrlParser: true})
 .then(res => {
-    console.log('Http Server Ready')
-    const server = app.listen(8080);
-
-    const io = require('./socket').init(server)
-
-    //whenever a new client connexts to us.
-    io.on("connection", socket => {
-        console.log('Client Connected')
-    })
+    console.log('Connected')
+    app.listen(8080);
 })
 .catch(err => {
     console.log('Connection Error', err)
